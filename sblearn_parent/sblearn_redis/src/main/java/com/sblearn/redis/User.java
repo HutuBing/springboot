fb12dbc09ad6590e86b85dc9049b3317c05ee35e
HutuBing <1161086097@qq.com>
2019-08-19 15:25:33 +0800

    redis

---
+++ sblearn_parent/sblearn_redis/src/main/java/com/sblearn/redis/User.java
@@ -0,0 +1,23 @@
+package com.sblearn.redis;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class User implements Serializable {
+    private String email;
+    private String account;
+    private String password;
+    private String userName;
+    private String desc;
+
+    public User(String email, String account, String password, String userName, String desc) {
+        this.email = email;
+        this.account = account;
+        this.password = password;
+        this.userName = userName;
+        this.desc = desc;
+    }
+
+}
